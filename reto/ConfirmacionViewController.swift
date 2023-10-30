f8eda989b90ff281d11ade4bc8d67241a1b73011
RogerHdzC <A01735819@tec.mx>
2023-10-30 17:19:52 -0600

    Initial Commit - Roger

---
+++ reto/ConfirmacionViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ConfirmacionViewController.swift
+//  reto
+//
+//  Created by Administrador on 30/10/23.
+//
+
+import UIKit
+import FirebaseAnalytics
+import FirebaseAuth
+
+enum ProvideType: String{
+    case basic
+}
+
+class ConfirmacionViewController: UIViewController{
+    
+    
+    @IBOutlet weak var emailLabel: UILabel!
+    
+    private let email: String
+    
+    init(email: String){
+        self.email = email
+        super.init(nibName: "ConfirmacionViewController", bundle: nil)
+    }
+    
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        emailLabel.text = email
+    }
+    
+    
+    @IBAction func logInAction(_ sender: Any) {
+    }
+    
+    
+}
---
+++ reto/InitViewController.swift
@@ -0,0 +1,31 @@
+//
+//  InitViewController.swift
+//  reto
+//
+//  Created by Administrador on 05/10/23.
+//
+
+import UIKit
+import FirebaseAnalytics
+
+class InitViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Analytics Event
+        Analytics.logEvent("InitScreen", parameters: ["message":"Integración de Firebase completa"])
+    }
+
+    @IBAction func logInAction(_ sender: Any) {
+        let viewLogIn = LogInViewController()
+        
+        self.navigationController?.pushViewController(viewLogIn, animated: true)
+    }
+    
+    @IBAction func signUpAction(_ sender: Any) {
+        let viewSignUp = SignUpViewController()
+        
+        self.navigationController?.pushViewController(viewSignUp, animated: true)
+    }
+}
+
